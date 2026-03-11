052ff4c6593ed7392d21df22f5ec456d9302ff91
Gilmara Ribeiro <gillmara.ribeeiro246@gmail.com>
2026-03-11 13:19:09 -0300

    corrigindo rota index

--- main.py
+++ main.py
@@ -1,82 +1,49 @@
 from fastapi import FastAPI, UploadFile, File
-from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
+from fastapi.responses import FileResponse
 from ultralytics import YOLO
+from PIL import Image
 import shutil
-import os
 
 app = FastAPI()
 
+# carregar modelo
 model = YOLO("best.pt")
 
-@app.get("/", response_class=HTMLResponse)
+# pasta static
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
+# abrir interface
+@app.get("/")
 def home():
-    return """
-    <html>
-    <head>
-        <title>Detector de Telhados</title>
-    </head>
-    <body>
+    return FileResponse("static/index.html")
 
-    <h2>Detector de Telhados</h2>
-
-    <input type="file" id="image">
-    <button onclick="enviar()">Analisar</button>
-
-    <pre id="resultado"></pre>
-
-    <script>
-    async function enviar(){
-
-        let file = document.getElementById("image").files[0]
-
-        let formData = new FormData()
-        formData.append("file", file)
-
-        let res = await fetch("/predict", {
-            method:"POST",
-            body:formData
-        })
-
-        let data = await res.json()
-
-        document.getElementById("resultado").innerText =
-        `Total de telhados: ${data.total}
-
-Telhados de cerâmica: ${data.ceramica}
-Telhados de brasilit: ${data.brasilit}`
-    }
-    </script>
-
-    </body>
-    </html>
-    """
 
 @app.post("/predict")
 async def predict(file: UploadFile = File(...)):
 
-    path = f"temp_{file.filename}"
+    caminho = f"temp_{file.filename}"
 
-    with open(path, "wb") as buffer:
+    with open(caminho, "wb") as buffer:
         shutil.copyfileobj(file.file, buffer)
 
-    results = model(path)
+    results = model(caminho)
 
+    total = 0
     ceramica = 0
     brasilit = 0
 
     for r in results:
         for c in r.boxes.cls:
+            total += 1
+
             if int(c) == 0:
                 ceramica += 1
             elif int(c) == 1:
                 brasilit += 1
 
-    total = ceramica + brasilit
-
-    os.remove(path)
-
     return {
-        "total": total,
-        "ceramica": ceramica,
-        "brasilit": brasilit
+        "Total de telhados": total,
+        "Telhados de cerâmica": ceramica,
+        "Telhados de brasilit": brasilit
     }
