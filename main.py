79993da92fabc3dd79d5b2e3981db41c99b1c030
Gilmara Ribeiro <gillmara.ribeeiro246@gmail.com>
2026-03-16 12:35:30 -0300

    teste modelo mais leve

--- main.py
+++ main.py
@@ -22,7 +22,7 @@ async def predict(file: UploadFile = File(...)):
 
     # carregar modelo somente quando analisar
     from ultralytics import YOLO
-    model = YOLO("best.pt")
+    model = YOLO("yolov8n.pt")
 
     contents = await file.read()
     image = Image.open(io.BytesIO(contents))
